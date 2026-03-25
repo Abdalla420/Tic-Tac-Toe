f71d49994c41e8d1634b65540757a1ad97981303
Abdalla420 <215827221+Abdalla420@users.noreply.github.com>
2026-03-24 23:18:17 -0400

    Create Gameboard as IIFE

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-function Gameboard(){
-    const rows = 3;
-    const columns = 3;
-    let board = [];
-
-    for(let i = 0; i < rows; i++){
-        board[i] = [];
-        for(let j = 0; j < columns; j++){
-            board[i][j] = Cell();
-        }
-    }
-    const getBoard = () => board;
-
-    const drawSymbol(row, column, player){
-
-    }
-}
-
-
-function Cell(){
-    let value = 0;
-
-    const addSymbol = (player) => {
-        value = player;
-    }
-
-    const getValue = () => value;
+const Gameboard = (function() {
+    let board = Array(9).fill(null);
     return {
-        addSymbol,
-        getValue
-    }
-}
+        mark(index, marker) {
+            if (board[index] === null){
+                board[index] = marker;
+                return true;
+            }else {
+                return false;
+            }
+        },
+        getBoard() {
+            return board;
+        },
+        reset() {
+            board = Array(9).fill(null);
+        }
+    };
+})();
+
