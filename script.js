2b3845ee6df78f6a087c5385b721e48ca29c5692
Abdalla420 <mamanbusiness420@gmail.com>
2026-03-29 19:02:59 -0400

    Create checkWin and checkDraw

--- script.js
+++ script.js
@@ -39,10 +39,63 @@ const Controller = (function(){
     let gameOver = false;
     return {
         play(index) {
-            if(Gameboard.mark(index, currentPlayer.marker) === true){
-                (currentPlayer === player1)? currentPlayer = player2 : currentPlayer = player1;
-            }
+            if (gameOver === false) {
+                // toggling turn 
+                if(Gameboard.mark(index, currentPlayer.marker) === true){
+                    if(this.checkWin() === false && this.checkDraw() === false){
+                        (currentPlayer === player1)? currentPlayer = player2 : currentPlayer = player1;
+                    }
+                };
+            }else {
 
+            }
         },
+        checkWin() {
+            let boardArray = Gameboard.getBoard();
+            const combinations = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+            const win = combinations.some(combo =>
+                boardArray[combo[0]] === boardArray[combo[1]] &&
+                boardArray[combo[1]] === boardArray[combo[2]] &&
+                boardArray[combo[0]] !== null
+            );
+            if (win === true){
+                gameOver = true;
+                return true;
+            }else {
+                return false;
+            }
+        },
+        checkDraw() {
+            let board = Gameboard.getBoard();
+            if(board.includes(null) === false) {
+                gameOver = true;
+                return true;
+            }else {
+                return false;
+            }
+        }
     }
 })();
+
+
+Controller.play(4);
+Controller.play(0);
+Controller.play(8);
+Controller.play(2);
+Controller.play(6);
+Controller.play(1);
+Gameboard.getBoard();
+// Controller.play(0);
+// Controller.play(0);
+// Controller.play(0);
+// Controller.play(0);
+
