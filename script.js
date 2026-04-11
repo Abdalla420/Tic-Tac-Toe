71c7aa10da3015eb4ad47733b66b847b6f2f8a42
Abdalla420 <215827221+Abdalla420@users.noreply.github.com>
2026-04-11 11:37:02 -0400

    Add DisplayController.displayBoard() in the right place

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ const Controller = (function(){
     let gameOver = false;
     return {
         play(index) {
-            DisplayController.getBoard();
             if (gameOver === false) {
                 // toggling turn 
                 if(Gameboard.mark(index, currentPlayer.marker) === true){
+                    DisplayController.displayBoard();
                     if(this.checkWin() === false && this.checkDraw() === false){
                         (currentPlayer === player1)? currentPlayer = player2 : currentPlayer = player1;
                     }
